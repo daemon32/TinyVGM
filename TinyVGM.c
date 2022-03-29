f87e48252daea95d92e89c7fc58c1280f97494a8
daemon32 <daemon32@users.noreply.github.com>
2022-03-28 19:14:04 -0700

    Initial live VGM support

--- TinyVGM.c
+++ TinyVGM.c
@@ -62,10 +62,6 @@ void tinyvgm_add_event_callback(TinyVGMContext *ctx, TinyVGMEvent event, int (*c
 	tinyvgm_add_callback(ctx, TinyVGM_CallBackType_Event, event, callback, userp);
 }
 
-void tinyvgm_add_header_callback(TinyVGMContext *ctx, uint8_t header_offset, int (*callback)(void *, uint8_t, const void *, uint32_t), void *userp) {
-	tinyvgm_add_callback(ctx, TinyVGM_CallBackType_Header, header_offset, callback, userp);
-}
-
 void tinyvgm_add_command_callback(TinyVGMContext *ctx, uint8_t command, int (*callback)(void *, uint8_t, const void *, uint32_t), void *userp) {
 	tinyvgm_add_callback(ctx, TinyVGM_CallBackType_Command, command, callback, userp);
 }
@@ -114,16 +110,12 @@ size_t tinyvgm_strlen16(const int16_t* strarg) {
 
 void tinyvgm_init(TinyVGMContext *ctx) {
 	memset(ctx, 0, sizeof(TinyVGMContext));
-	ctx->header_info.data_offset = 0x00000040;
 }
 
 void tinyvgm_reset(TinyVGMContext *ctx) {
 	ctx->read_bytes = 0;
 	ctx->buffer_pos = 0;
 	ctx->current_command = 0;
-
-	memset(&ctx->header_info, 0, sizeof(TinyVGMHeaderInfo));
-	ctx->header_info.data_offset = 0x00000040;
 }
 
 void tinyvgm_destroy(TinyVGMContext *ctx) {
@@ -138,155 +130,37 @@ int32_t tinyvgm_parse(TinyVGMContext *ctx, const void *buf, uint16_t len) {
 	for (uint16_t i = 0; i < len; i++) {
 		uint8_t cur_byte = ((uint8_t *) buf)[i];
 
-		// TODO Header Len Chk
-		/*
-		typedef struct timyvgm_header_info {
-			uint32_t eof_offset;	// 0x04 - 0x07
-			uint32_t version;	// 0x08 - 0x11
-			uint32_t gd3_offset;	// 0x14 - 0x17
-			uint32_t total_samples;	// 0x18 - 0x1b
-			uint32_t loop_offset;	// 0x1c - 0x1f
-			uint32_t loop_samples;	// 0x20 - 0x23
-			uint32_t rate;		// 0x24 - 0x27
-			uint32_t data_offset;	// 0x34 - 0x37
-		} TinyVGMHeaderInfo;
-		 */
-		if (ctx->read_bytes < ctx->header_info.data_offset) {
-			if (tinyvgm_buffer_push(ctx, cur_byte) == 4) {
-				if (ctx->read_bytes == 0x03) {
-					static const char hdr[] = "Vgm ";
-					if (memcmp(ctx->buffer, hdr, 4) != 0) {
-						return INT32_MIN;
-					}
-				} else if (ctx->read_bytes == 0x07) {
-					memcpy(&ctx->header_info.eof_offset, ctx->buffer, 4);
-				} else if (ctx->read_bytes == 0x11) {
-					// TODO:
-					// 1. Parse BCD-version
-					memcpy(&ctx->header_info.version, ctx->buffer, 4);
-				} else if (ctx->read_bytes == 0x17) {
-					memcpy(&ctx->header_info.gd3_offset, ctx->buffer, 4);
-					ctx->header_info.gd3_offset += 0x14;
-				} else if (ctx->read_bytes == 0x1b) {
-					memcpy(&ctx->header_info.total_samples, ctx->buffer, 4);
-				} else if (ctx->read_bytes == 0x1f) {
-					memcpy(&ctx->header_info.loop_offset, ctx->buffer, 4);
-					ctx->header_info.loop_offset += 0x1c;
-				} else if (ctx->read_bytes == 0x23) {
-					memcpy(&ctx->header_info.loop_samples, ctx->buffer, 4);
-				} else if (ctx->read_bytes == 0x27) {
-					memcpy(&ctx->header_info.rate, ctx->buffer, 4);
-				} else if (ctx->read_bytes == 0x37) {
-					uint32_t data_offset = *((uint32_t *) ctx->buffer);
+		tinyvgm_invoke_callback(ctx, TinyVGM_CallBackType_Event, TinyVGM_Event_HeaderParseDone, NULL, 0);
 
-					// TODO:
-					// 1. Check version first?
-					if (data_offset != 0 && data_offset != 0x0000000c) {
-						memcpy(&ctx->header_info.data_offset, ctx->buffer, 4);
-						ctx->header_info.data_offset += 0x34;
-					}
+		if (!ctx->current_command) {
+			if (vgm_cmd_length_table[cur_byte] == 0) {
+				if (TinyVGM_OK != tinyvgm_invoke_callback(ctx, TinyVGM_CallBackType_Command, cur_byte, NULL, 0)) {
+					return -(i+1);
 				}
-
-				tinyvgm_invoke_callback(ctx, TinyVGM_CallBackType_Header, ctx->read_bytes - 3, ctx->buffer, 4);
-				tinyvgm_buffer_clear(ctx);
-			}
-
-			if (ctx->read_bytes == (ctx->header_info.data_offset - 1)) {
-				tinyvgm_invoke_callback(ctx, TinyVGM_CallBackType_Event, TinyVGM_Event_HeaderParseDone, NULL, 0);
+			} else if (vgm_cmd_length_table[cur_byte] == -1) {
+				// Do nothing
+				printf("FATAL: Unknown command: 0x%02x\n", cur_byte);
+//				abort();
+			} else if (vgm_cmd_length_table[cur_byte] == -2) {
+				// TODO
+			} else {
+				ctx->current_command = cur_byte;
 			}
 		} else {
-			if (!ctx->current_command) {
-				if (vgm_cmd_length_table[cur_byte] == 0) {
-					if (TinyVGM_OK != tinyvgm_invoke_callback(ctx, TinyVGM_CallBackType_Command, cur_byte, NULL, 0)) {
-						return -(i+1);
-					}
-				} else if (vgm_cmd_length_table[cur_byte] == -1) {
-					// Do nothing
-					printf("FATAL: Unknown command: 0x%02x\n", cur_byte);
-//					abort();
-				} else if (vgm_cmd_length_table[cur_byte] == -2) {
-					// TODO
+			if (tinyvgm_buffer_push(ctx, cur_byte) == vgm_cmd_length_table[ctx->current_command]) {
+				int rc = tinyvgm_invoke_callback(ctx, TinyVGM_CallBackType_Command, ctx->current_command, ctx->buffer, ctx->buffer_pos);
+				if (rc == TinyVGM_OK) {
+					tinyvgm_buffer_clear(ctx);
 				} else {
-					ctx->current_command = cur_byte;
-				}
-			} else {
-				if (tinyvgm_buffer_push(ctx, cur_byte) == vgm_cmd_length_table[ctx->current_command]) {
-					int rc = tinyvgm_invoke_callback(ctx, TinyVGM_CallBackType_Command, ctx->current_command, ctx->buffer, ctx->buffer_pos);
-
-					if (rc == TinyVGM_OK) {
-						tinyvgm_buffer_clear(ctx);
-					} else {
-						return -(i+1);
-					}
+					return -(i+1);
 				}
 			}
 		}
-
-		if (ctx->read_bytes == ctx->header_info.gd3_offset - 1) {
-			tinyvgm_invoke_callback(ctx, TinyVGM_CallBackType_Event, TinyVGM_Event_PlaybackDone, NULL, 0);
-			return i;
-		}
-
-		ctx->read_bytes++;
 	}
+
+	ctx->read_bytes++;
 
 	return len;
-}
-
-void tinyvgm_init_gd3(TinyVGMGd3Info *ctx) {
-	memset(ctx, 0, sizeof(TinyVGMGd3Info));
-}
-
-void tinyvgm_destroy_gd3(TinyVGMGd3Info *ctx) {
-	if (ctx->buf) {
-		free(ctx->buf);
-		memset(ctx, 0, sizeof(TinyVGMGd3Info));
-	}
-}
-
-int32_t tinyvgm_parse_gd3(TinyVGMGd3Info *ctx, const void *buf, uint16_t len) {
-	uint16_t read_len = 0;
-
-	while (read_len < len) {
-		if (ctx->__read_bytes < 8) {
-			read_len += 1;
-			ctx->__read_bytes += 1;
-		} else if (ctx->__read_bytes >= 8 && ctx->__read_bytes < 12) {
-			((uint8_t*)&ctx->__total_length)[ctx->__read_bytes - 8] = *((uint8_t*)buf + read_len);;
-			read_len += 1;
-			ctx->__read_bytes += 1;
-
-			if (ctx->__read_bytes == 12) {
-				ctx->buf = (int16_t *)malloc(ctx->__total_length);
-			}
-		} else if (ctx->__read_bytes >= 12 && ctx->__read_bytes - 12 < ctx->__total_length) {
-			uint16_t cpy_length = len - read_len;
-			if (ctx->__read_bytes - 12 + cpy_length > ctx->__total_length) {
-				cpy_length -= ctx->__read_bytes - 12 + cpy_length - ctx->__total_length;
-			}
-			memcpy((uint8_t *)ctx->buf + (ctx->__read_bytes - 12), ((uint8_t*)buf + read_len), cpy_length);
-			read_len += cpy_length;
-			ctx->__read_bytes += cpy_length;
-
-			if (ctx->__read_bytes - 12 == ctx->__total_length) {
-				ctx->title = ctx->buf;
-				ctx->title_jp = ctx->title + tinyvgm_strlen16(ctx->title) + 1;
-				ctx->album = ctx->title_jp + tinyvgm_strlen16(ctx->title_jp) + 1;
-				ctx->album_jp = ctx->album + tinyvgm_strlen16(ctx->album) + 1;
-				ctx->system_name = ctx->album_jp + tinyvgm_strlen16(ctx->album_jp) + 1;
-				ctx->system_name_jp = ctx->system_name + tinyvgm_strlen16(ctx->system_name) + 1;
-				ctx->composer = ctx->system_name_jp + tinyvgm_strlen16(ctx->system_name_jp) + 1;
-				ctx->composer_jp = ctx->composer + tinyvgm_strlen16(ctx->composer) + 1;
-				ctx->release_date = ctx->composer_jp + tinyvgm_strlen16(ctx->composer_jp) + 1;
-				ctx->converter = ctx->release_date + tinyvgm_strlen16(ctx->release_date) + 1;
-				ctx->note = ctx->converter + tinyvgm_strlen16(ctx->converter) + 1;
-			}
-		} else {
-			return read_len;
-		}
-	}
-
-	return read_len;
 }
 
 #ifdef __cplusplus
--- TinyVGM.h
+++ TinyVGM.h
@@ -41,28 +41,8 @@ typedef enum {
 
 typedef enum {
 	TinyVGM_CallBackType_Command = 0,
-	TinyVGM_CallBackType_Header = 1,
 	TinyVGM_CallBackType_Event = 2
 } TinyVGMCallbackType;
-
-typedef struct tinyvgm_gd3_info {
-	uint32_t __read_bytes;
-	uint32_t __total_length;
-
-	int16_t* title;
-	int16_t* title_jp;
-	int16_t* album;
-	int16_t* album_jp;
-	int16_t* system_name;
-	int16_t* system_name_jp;
-	int16_t* composer;
-	int16_t* composer_jp;
-	int16_t* release_date;
-	int16_t* converter;
-	int16_t* note;
-
-	int16_t* buf;
-} TinyVGMGd3Info;
 
 typedef struct tinyvgm_callback_info {
 	uint8_t id;
@@ -71,22 +51,9 @@ typedef struct tinyvgm_callback_info {
 	int (*callback)(void *, uint8_t, const void *, uint32_t);
 } TinyVGMCallbackInfo;
 
-typedef struct timyvgm_header_info {
-	uint32_t eof_offset;	// 0x04
-	uint32_t version;	// 0x08
-	uint32_t gd3_offset;	// 0x14
-	uint32_t total_samples;	// 0x18
-	uint32_t loop_offset;	// 0x1c
-	uint32_t loop_samples;	// 0x20
-	uint32_t rate;		// 0x24
-	uint32_t data_offset;	// 0x34
-} TinyVGMHeaderInfo;
-
 typedef struct tinyvgm_context {
 	TinyVGMCallbackInfo *callbacks[3];
 	uint8_t nr_callbacks[3];
-
-	TinyVGMHeaderInfo header_info;
 
 	uint32_t read_bytes;
 
@@ -155,17 +122,6 @@ extern int32_t tinyvgm_parse(TinyVGMContext *ctx, const void *buf, uint16_t len)
 extern void tinyvgm_add_event_callback(TinyVGMContext *ctx, TinyVGMEvent event, int (*callback)(void *, uint8_t, const void *, uint32_t), void *userp);
 
 /**
- * Add a header callback.
- *
- * @param ctx			TinyVGM context pointer.
- * @param header_offset		Header offset. e.g. 0x08 for `Version number'.
- * @param callback		Callback function pointer.
- * @param userp			User data pointer to supply as the first argument of the callback function. Can be a C++ class pointer or NULL.
- *
- */
-extern void tinyvgm_add_header_callback(TinyVGMContext *ctx, uint8_t header_offset, int (*callback)(void *, uint8_t, const void *, uint32_t), void *userp);
-
-/**
  * Add a command callback.
  *
  * @param ctx			TinyVGM context pointer.
@@ -175,36 +131,6 @@ extern void tinyvgm_add_header_callback(TinyVGMContext *ctx, uint8_t header_offs
  *
  */
 extern void tinyvgm_add_command_callback(TinyVGMContext *ctx, uint8_t command, int (*callback)(void *, uint8_t, const void *, uint32_t), void *userp);
-
-/**
- * Initialize a TinyVGM GD3 context.
- *
- * @param ctx			TinyVGM GD3 context pointer.
- *
- */
-extern void tinyvgm_init_gd3(TinyVGMGd3Info *ctx);
-
-/**
- * Destroy a TinyVGM GD3 context, frees all runtime allocated memory.
- *
- * @param ctx			TinyVGM GD3 context pointer.
- *
- */
-extern void tinyvgm_destroy_gd3(TinyVGMGd3Info *ctx);
-
-/**
- * Parse a portion of GD3 stream.
- *
- * No data validation will be performed at this time.
- *
- * @param ctx			TinyVGM GD3 context pointer.
- * @param buf			Buffer of the GD3 stream.
- * @param len			Length of buffer.
- *
- * @return			Length of successfully processed bytes.
- *
- */
-extern int32_t tinyvgm_parse_gd3(TinyVGMGd3Info *ctx, const void *buf, uint16_t len);
 
 #ifdef __cplusplus
 };
